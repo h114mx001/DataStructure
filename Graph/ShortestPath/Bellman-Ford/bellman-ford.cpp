336b7cd3e8fb26effda4840ec2ff374cc89261e7
Tuan Anh Nguyen <flyablecat179@gmail.com>
2021-01-22 01:25:27 +0700

    comment for bellman ford algo

--- Graph/ShortestPath/Bellman-Ford/bellman-ford.cpp
+++ Graph/ShortestPath/Bellman-Ford/bellman-ford.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 bool stop = false;
+bool negativeCycle = false;
 int distance[100];
 int edges, nodes, start, finish;
-int tracer[100];
-std::vector<std::tuple<int,int,int>> edgList;
+int tracer[100]; //Trace the optimal from a node to another node.
+std::vector<std::tuple<int,int,int>> edgList; //Edge list
 void IOInit(){
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
     freopen("graph.inp","r",stdin);
 }
 
-void graphRead(){
+void graphRead(){ //Read the graph as a edge list.
     std::cin >> nodes >> edges >> start >> finish;
     int a, b, weight;
     for (int i = 0; i < edges; i++){
@@ -20,21 +21,28 @@ void graphRead(){
     }
 }
 
-void BellmanFord(){
-    for (int i = 1; i <= nodes; i++) distance[i] = 9999999;
-    distance[start] = 0;
-    for (int i = 1; i <= nodes - 1; i++){
+void BellmanFord(){ 
+    for (int i = 1; i <= nodes; i++) distance[i] = 9999999; //Init every node has infinity distance
+    distance[start] = 0; //except the start node
+    for (int i = 1; i <= nodes - 1; i++){ //Bellman-Ford algo
         stop = true;
+        //The boolean plays two roles.
+        //1. It will stop the algorithm if it can not make any distances smaller
+        //2. It can identify whether the graph has a negative cycle or not.
         for (auto e: edgList){
             int a, b, w;
-            std::tie(a,b,w) = e;
-            if (distance[b] > distance[a] + w){
+            std::tie(a,b,w) = e; //unpack the tuple (other way, the edge)
+            if (distance[b] > distance[a] + w){ //Check whether the algo can reduce the distance. 
                 distance[b] = distance[a] + w;
-                tracer[b] = a;
-                stop = false;
+                tracer[b] = a; //The optimal node to go to
+                stop = false; //We should not stop the algorithm because it can reduce more
             }
         }
-        if (stop) return;
+        if (i == nodes - 1){ //If the algo have to run fully, graph contains a negative cycle
+            negativeCycle = true;
+            return;
+        }
+        if (stop) return; //If there is a negative cycle, or the algo can't reduce anymore
     }
 }
 
@@ -42,18 +50,19 @@ int main(){
     IOInit();
     graphRead();
     BellmanFord();
-    if (stop == false){
+    if (negativeCycle){
         std::cout << "There is a negative cycle in this graph. \n";
     }
     else{
-    if (distance[finish] == 9999999){
+    if (distance[finish] == 9999999){ //If we can not reduce the distance to the finish node
             std::cout << "There aren't any from " << start << " to " << finish << ".\n";
         }
-        else{
+        else{ //Otherwise, we found a path
             std::cout << "Shortest path from " << start << " to " << finish << ": \n";
-            while (start != finish){
+            std::cout << "With the cost: " << distance[finish] << "\n";
+            while (start != finish){ //while we reach the finish node
                 std::cout << finish << "<-";
-                finish = tracer[finish];
+                finish = tracer[finish]; //We trace to the best node to go to 
             }
             std::cout << start << "\n";
         }
